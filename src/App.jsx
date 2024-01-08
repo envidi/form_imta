85b5c397ea5cd7e0bdf1abf50472ac680ead1257
envidi <phanm711996@gmail.com>
2024-01-08 19:40:16 +0700

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,40 @@
+import { useState } from "react";
+import Button from "./components/Button";
+
+import Essay from "./components/Essay";
+import Topic from "./components/Topic";
+function App() {
+  const [currentChoice, setCurrentChoice] = useState("topic");
+
+  const handleSelectChoicEssay = () => {
+    setCurrentChoice("essay");
+  };
+  const handleSelectChoicTopic = () => {
+    setCurrentChoice("topic");
+  };
+  return (
+    <>
+      <div className='w-100 flex flex-col px-[4.1%] bg-slate-100'>
+        <div className='flex xs:flex-col sm:flex-row  xs:w-100 justify-between sm:gap-5 xs:gap-2'>
+          <Button
+            type={currentChoice === "topic" ? "active" : "outline"}
+            handleClick={handleSelectChoicTopic}
+            className={` sm:w-1/2 xs:w-100 xs:py-2`}
+          >
+            Writing Topic
+          </Button>
+          <Button
+            type={currentChoice === "essay" ? "active" : "outline"}
+            handleClick={handleSelectChoicEssay}
+            className={` sm:w-1/2 xs:w-100 xs:py-2`}
+          >
+            From Your Essay
+          </Button>
+        </div>
+        {currentChoice === "essay" ? <Essay /> : <Topic />}
+      </div>
+    </>
+  );
+}
+
+export default App;
